2e0f943b4c51539b48bb7ca12bdafdc29f86f999
Brendan Jobus <brendanjobus@gmail.com>
2021-02-18 02:56:23 +0000

    Created a sandbox folder, which currently contains a basic server and a basic client, they are very simple, the server will continue to run until stopped and the client will run until a message is inputed. The data they currently transfer is a simple string, however, this can be easily changed.

---
+++ sandbox/basicServer.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <thread>
+
+#define DEFAULT_PORT 2099
+
+void acceptFunc(int sockfd) {
+  struct sockaddr_in cli_addr;
+  socklen_t clilen = sizeof(cli_addr);
+
+  while(1) {
+    int n;
+    char buffer[255];
+    bzero((char *) buffer, sizeof(buffer));
+
+    int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) { perror("ERROR on accept"); exit(EXIT_FAILURE); }
+
+    printf("server: got connection from %s port %d\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+    send(newsockfd, "Hello, world!\n", 13, 0);
+
+    n = read(newsockfd, buffer, 255);
+    if (n < 0) { perror("ERROR reading from socket\n"); exit(EXIT_FAILURE); }
+    printf("Here is the message: %s\n", buffer);
+    close(newsockfd);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int sockfd, newsockfd, portno;
+  int sockbinding, opt = 1;
+
+  socklen_t clilen;
+  char buffer[255];
+
+  struct sockaddr_in serv_addr;
+  int n;
+
+  if (argc < 2) {
+    printf("usage: %s port\nNow switching to default mode\n", argv[0]);
+    portno = DEFAULT_PORT;
+  } else {
+    portno = atoi(argv[1]);
+  }
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) { perror("ERROR opening socket\n"); exit(EXIT_FAILURE); }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    perror("ERROR on binding\n");
+    exit(EXIT_FAILURE);
+  }
+
+  listen(sockfd, 5);
+  std::thread t(acceptFunc, sockfd);
+  t.join();
+
+  close(sockfd);
+  return 0;
+}
